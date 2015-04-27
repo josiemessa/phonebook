cb3f4830585089107e218b319e48a39834695289
Josie Messa <josie.messa@uk.ibm.com>
2015-04-27 09:47:11 +0100

    Clean up main code

--- main.js
+++ main.js
@@ -6,25 +6,27 @@ var phonebookFile = 'phonebook.json';
 var allowedEntries = ["Surname", "Firstname", "Phone", "Address"];
 
 // Keep this synchronous as we'll be useless without the data loaded
+console.log("Reading in phonebook");
 var phonebook = JSON.parse(fs.readFileSync(phonebookFile, 'utf8'));
 var app = express();
 app.use(bodyParser.json());
 
 app.get('/phonebook', function(req, res) {
   // only go down this path if the "surname" query parameter was used
-  if(req.query && null != req.query.surname) {
+  if(req.query && (null != req.query.surname)) {
     // Look through phonebook for matching surnames
     var responseBody = {};
     for (var entry in phonebook) {
-      if(phonebook.hasOwnProperty(entry.toString())) {
-        if(phonebook[entry.toString()].Surname == req.query.surname) {
-          responseBody[entry.toString()] = phonebook[entry.toString()];
+      if(phonebook.hasOwnProperty(entry)) {
+        if(phonebook[entry].Surname == req.query.surname) {
+          responseBody[entry] = phonebook[entry]; // add entry to response
         }
       }
     }
     res.send(responseBody);
     return;
   } else {
+    // If no query parameters were used then send the entire phonebook back
     res.send(phonebook);
     return;
   }
@@ -40,7 +42,7 @@ app.post('/phonebook', function(req, res){
     return;
   }
 
-  // Check any other fields supplied are in the accepted list
+  // Check no other fields were supplied other than those that can be stored in phonebook
   for (var property in newEntry) {
     if (newEntry.hasOwnProperty(property)) {
       if (allowedEntries.indexOf(property) == -1) {
@@ -51,18 +53,21 @@ app.post('/phonebook', function(req, res){
   }
 
   // Error checking done, let's store some data!
-  var keysAsInt = Object.keys(phonebook).map(function(key){
+  var keysAsInt = Object.keys(phonebook).map(function(key){ // Convert phonebook entry keys to int
     return parseInt(key);
-  }); // Convert keys to int
-  var newKey = Math.max.apply(null, keysAsInt) + 1; // New key increments current highest key
-  phonebook[newKey.toString()] = newEntry;
+  }); 
+  var newKey = Math.max.apply(null, keysAsInt) + 1; // Create new key to store new phonebook entry
+  phonebook[newKey.toString()] = newEntry; 
 
+  // Store new entry
+  console.log("Storing new entry in phonebook...");
   fs.writeFile(phonebookFile, JSON.stringify(phonebook), function(err){
     if(err) {
-      console.log(err);
+      console.log("**ERROR** "+err);
       res.status(500).send({"Error": "Could not store new entry in phonebook"});
       return;
     }
+    console.log("...New entry stored with key "+newKey);
     res.status(201).location("http://localhost:3000/phonebook/"+newKey.toString()).send();
     return;
   })
@@ -81,19 +86,21 @@ app.put('/phonebook/:id', function(req, res){
 
   for(var property in newEntry){
     if(newEntry.hasOwnProperty(property)){
-      // Check if the properties being updated are valid things to update
+      // Check supplied fields are valid for a phonebook entry
       if(allowedEntries.indexOf(property) == -1) {
         res.status(400).send({"Error": "Phonebook entries can only include the following mandatory fields: 'Surname', 'Firstname', 'Phone', and optionally: 'Address'"});
         return;
       }
-      // All good! Update the property
+      // All good! Update the supplied property
       phonebook[id][property] = newEntry[property];
+      console.log("Updating phonebook entry "+id+"...");
       fs.writeFile(phonebookFile, JSON.stringify(phonebook), function(err){
       if(err) {
-        console.log(err);
+        console.log("**ERROR** "+err);
         res.status(500).send({"Error": "Could not update entry in phonebook"});
         return;
       }
+      console.log("...Phonebook entry updated with id "+id);
       res.status(201).location("http://localhost:3000/phonebook/"+id).send();
       return;
     })
@@ -112,17 +119,18 @@ app.delete('/phonebook/:id', function(req, res){
 
   // Get deleting
   delete phonebook[id];
-  // update phonebook file
+  // Update phonebook file
+  console.log("Deleting phonebook entry "+id+"...");
   fs.writeFile(phonebookFile, JSON.stringify(phonebook), function(err){
       if(err) {
-        console.log(err);
+        console.log("**ERROR** "+err);
         res.status(500).send({"Error": "Could not delete entry in phonebook"});
         return;
       }
   });
+  console.log("...Phonebook entry deleted with id "+id);
   res.sendStatus(204);
 });
 
-
-
+console.log("Starting Express server on port 3000");
 app.listen(3000);
